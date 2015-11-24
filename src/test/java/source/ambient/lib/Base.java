9b48e64b39a84800ba80230d1ac72b3436b5f9aa
Renaud Pawlak <renaud.pawlak@gmail.com>
2015-11-24 10:09:50 +0100

    updated copyright

--- src/test/java/source/ambient/lib/Base.java
+++ src/test/java/source/ambient/lib/Base.java
@@ -1,4 +1,6 @@
-/* Copyright 2015 CINCHEO SAS
+/* 
+ * JSweet - http://www.jsweet.org
+ * Copyright (C) 2015 CINCHEO SAS <renaud.pawlak@cincheo.fr>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
