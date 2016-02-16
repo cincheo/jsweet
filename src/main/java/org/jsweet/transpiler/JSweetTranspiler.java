33fec2bb4121a1aff150860c1255b944814acfeb
Renaud Pawlak <renaud.pawlak@gmail.com>
2016-02-16 11:01:08 +0100

    removed useless install

--- src/main/java/org/jsweet/transpiler/JSweetTranspiler.java
+++ src/main/java/org/jsweet/transpiler/JSweetTranspiler.java
@@ -246,9 +246,6 @@ public class JSweetTranspiler implements JSweetOptions {
 		}
 		if (!ProcessUtil.isInstalledWithNpm("browserify")) {
 			ProcessUtil.installNodePackage("browserify", true);
-			// make sure that jquery is installed because sometime it fails to
-			// be installed
-			ProcessUtil.installNodePackage("jquery", true);
 		}
 	}
 
