9986baadb245d9caf4c0972af2a3391654669961
Renaud Pawlak <renaud.pawlak@gmail.com>
2016-05-06 07:50:39 +0200

    Java string API test

---
+++ src/test/java/source/api/Strings.java
@@ -0,0 +1,27 @@
+package source.api;
+
+import static jsweet.util.Globals.$export;
+
+import jsweet.lang.Array;
+
+public class Strings {
+
+	static Array<String> trace = new Array<String>();
+
+	public static void main(String[] args) {
+		byte[] bytes = "abc".getBytes();
+		char[] chars = "abc".toCharArray();
+		String s1 = new String(bytes, 1, 1);
+		trace.push(s1);
+		trace.push("abc".substring(1, 3));
+		trace.push("abc".substring(2));
+		String s2 = new String(chars, 1, 2);
+		trace.push(s2);
+		trace.push(String.valueOf("abc".length()));
+		trace.push(String.valueOf(!"abc".isEmpty()));
+		trace.push(String.valueOf("abc".subSequence(0, 2)));
+		trace.push(String.valueOf("x x".codePointAt(1)));
+		$export("trace", trace.join(","));
+	}
+
+}
