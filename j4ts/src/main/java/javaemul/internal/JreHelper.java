ec5bf556a4e6b5c3752d499a0cfff74a02b9b012
Louis Grignon <louis.grignon@gmail.com>
2017-04-28 02:57:07 +0200

    delete j4ts (official repo is https://github.com/j4ts/j4ts/tree/develop/v0.5.0)

--- j4ts/src/main/java/javaemul/internal/JreHelper.java
+++
@@ -1,9 +0,0 @@
-package javaemul.internal;
-
-import def.js.Math;
-
-public class JreHelper {
-
-	public static double LOG10E = Math.LOG10E;
-	
-}
--- j4ts/src/main/java/javaemul/internal/globals/Globals.java
+++
@@ -1,9 +0,0 @@
-package javaemul.internal.globals;
-import jsweet.lang.Ambient;
-
-public class Globals {
-
-	@Ambient
-	public static def.js.Object Map;
-	
-}
