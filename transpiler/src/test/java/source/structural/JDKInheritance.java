e39ded506f3cbbfd79ad407e1789cd971817d0f3
Renaud Pawlak <renaud.pawlak@gmail.com>
2017-05-11 16:49:05 +0200

    forgotten test

---
+++ transpiler/src/test/java/source/structural/JDKInheritance.java
@@ -0,0 +1,21 @@
+package source.structural;
+
+import java.util.ArrayList;
+
+@SuppressWarnings("serial")
+public class JDKInheritance extends ArrayList<String> {
+
+    public int getI() {
+	return 7;
+    }
+
+    public static void main(String[] args) {
+
+	JDKInheritance i = new JDKInheritance();
+	i.add("a");
+	assert i.contains("a") && i.size() == 1;
+	assert i.getI() == 7;
+
+    }
+
+}
