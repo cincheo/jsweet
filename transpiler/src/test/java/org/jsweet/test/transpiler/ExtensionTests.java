83125a6af62331cea20341685bf16d178757fbef
Renaud Pawlak <renaud.pawlak@gmail.com>
2017-05-19 09:15:07 +0200

    added cases for extension using the @Name annotation

--- transpiler/src/test/java/org/jsweet/test/transpiler/ExtensionTests.java
+++ transpiler/src/test/java/org/jsweet/test/transpiler/ExtensionTests.java
@@ -1,5 +1,7 @@
 package org.jsweet.test.transpiler;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsweet.transpiler.JSweetContext;
 import org.jsweet.transpiler.JSweetFactory;
 import org.jsweet.transpiler.ModuleKind;
@@ -28,6 +30,10 @@ class TestAdapter extends Java2TypeScriptAdapter {
 	public TestAdapter(JSweetContext context) {
 		super(context);
 		context.addAnnotation("@Erased", "**.toBeErased(..)");
+		context.addAnnotation("@Name('_f2')", "**.f2");
+		context.addAnnotation("@Name('_f4')", "**.f4(..)");
+		context.addAnnotation("@Name('_f6')", "**.f6");
+		context.addAnnotation("@Name('_f8')", "**.f8(..)");
 	}
 
 }
@@ -48,6 +54,20 @@ public class ExtensionTests extends AbstractTest {
 	public void testAnnotations() {
 		transpile(ModuleKind.none, (logHandler) -> {
 			Assert.assertEquals("There should be no errors", 0, logHandler.reportedProblems.size());
+			try {
+				String generated = FileUtils.readFileToString(transpiler.getContext().sourceFiles[0].getTsFile());
+				Assert.assertEquals(3, StringUtils.countMatches(generated, "_f1"));
+				Assert.assertEquals(3, StringUtils.countMatches(generated, "_f2"));
+				Assert.assertEquals(2, StringUtils.countMatches(generated, "_f3"));
+				Assert.assertEquals(2, StringUtils.countMatches(generated, "_f4"));
+				Assert.assertEquals(3, StringUtils.countMatches(generated, "_f5"));
+				Assert.assertEquals(3, StringUtils.countMatches(generated, "_f6"));
+				Assert.assertEquals(2, StringUtils.countMatches(generated, "_f7"));
+				Assert.assertEquals(2, StringUtils.countMatches(generated, "_f8"));
+			} catch (Exception e) {
+				e.printStackTrace();
+				Assert.fail();
+			}
 		}, getSourceFile(AnnotationTest.class));
 	}
 
--- transpiler/src/test/java/source/extension/AnnotationTest.java
+++ transpiler/src/test/java/source/extension/AnnotationTest.java
@@ -1,10 +1,47 @@
 package source.extension;
 
-public class AnnotationTest {
+import jsweet.lang.Name;
+
+class Superclass {
+	@Name("_f1")
+	String f1;
+	String f2;
+	@Name("_f3")
+	String f3() {
+		return "";
+	}
+	String f4() {
+		return "";
+	}
+}
+
+public class AnnotationTest extends Superclass {
 
 	public void toBeErased() {
 		// this will not be transpiled because the method will be erased
 		javax.activity.InvalidActivityException exception = null;
 	}
 
+	public void m() {
+		String s = f1;
+		s = f2;
+		s = f3();
+		s = f4();
+		s = f5;
+		s = f6;
+		s = f7();
+		s = f8();
+	}
+	
+	@Name("_f5")
+	String f5;
+	String f6;
+	@Name("_f7")
+	String f7() {
+		return "";
+	}
+	String f8() {
+		return "";
+	}
+	
 }
