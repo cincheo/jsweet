1a13d43ebbbdf7e240ec929e9d0b323beaf32e02
Louis Grignon <louis.grignon@gmail.com>
2018-10-07 12:44:13 +0200

    example for #487

--- transpiler/src/test/java/source/syntax/LambdaExpression.java
+++ transpiler/src/test/java/source/syntax/LambdaExpression.java
@@ -2,8 +2,14 @@ package source.syntax;
 
 import jsweet.util.Lang;
 
+@FunctionalInterface
+interface MyFunct {
+    double combine(double a, double b);
+}
+
 public class LambdaExpression {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		Lang.function(() -> {
@@ -13,7 +19,15 @@ public class LambdaExpression {
 		Lang.$apply(Lang.function(() -> {
 			System.out.println("hello");
 		}));
-
+		
+	    MyFunct SUM2 = new MyFunct() {
+	        @Override
+	        public double combine(double a, double b) {
+	            return a + b;
+	        }
+	    };
+	    MyFunct SUM = (a, b) -> a + b;
+	    MyFunct MAX = Math::max;
 	}
 
 }
