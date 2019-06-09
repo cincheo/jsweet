de0931843a56ee3578188485c6527641681511d6
Louis Grignon <louis.grignon@gmail.com>
2019-06-09 23:15:50 +0200

    ImageData.data is a UInt8Array to better match API

--- core-lib/es6/src/main/java/def/dom/ImageData.java
+++ core-lib/es6/src/main/java/def/dom/ImageData.java
@@ -1,12 +1,12 @@
 package def.dom;
 
-import def.js.Object;
+import def.js.Uint8Array;
 
 public class ImageData extends def.js.Object {
-    public double[] data;
     public double height;
     public double width;
     public static ImageData prototype;
     public ImageData(){}
+    public Uint8Array data;
 }
 
