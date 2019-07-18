55e6983f5660df58736cad02835d32f41cfa8976
lgrignon <louis.grignon@idcapture.fr>
2019-07-18 20:38:45 +0200

    ImageData up to date constructor

--- core-lib/es6/src/main/java/def/dom/ImageData.java
+++ core-lib/es6/src/main/java/def/dom/ImageData.java
@@ -1,12 +1,22 @@
 package def.dom;
 
 import def.js.Uint8Array;
+import def.js.Uint8ClampedArray;
 
 public class ImageData extends def.js.Object {
-    public double height;
-    public double width;
-    public static ImageData prototype;
-    public ImageData(){}
-    public Uint8Array data;
-}
+	public int height;
+	public int width;
 
+	public static ImageData prototype;
+
+	public Uint8Array data;
+
+	public ImageData() {
+	}
+
+	public ImageData(Uint8ClampedArray array, int width, int height) {
+	}
+
+	public ImageData(int width, int height) {
+	}
+}
