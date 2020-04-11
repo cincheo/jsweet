9982e55a1c50f8787c911303472802249160ca53
Renaud Pawlak <renaud.pawlak@gmail.com>
2020-04-11 11:47:32 +0200

    fix #574: regression with imports on enums

---
+++ transpiler/src/test/java/source/enums/SwitchWithEnumWrapper.java
@@ -0,0 +1,21 @@
+package source.enums;
+
+import source.enums.other.EnumWrapper;
+
+public class SwitchWithEnumWrapper {
+
+	public static void main(String[] args) {
+		assert test(new EnumWrapper()).equals("V1");
+	}
+
+	public static String test(EnumWrapper wrapper) {
+		switch(wrapper.getEnum()) {
+		case V1:
+			return "V1";
+		case V2:
+			return "V2";
+		}
+		return "UNMATCHED";
+	}
+	
+}
